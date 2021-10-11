d839214b4ed466a14e4dd4af99cc5b24b2f2f3f5
wwwwwwOwO <1463730297@qq.com>
2021-10-12 01:14:24 +0800

    init

---
+++ Materials/controllers/main/build/release/main.d
@@ -0,0 +1,6 @@
+build/release/main.o: main.cpp \
+/usr/local/webots/include/controller/cpp/webots/Robot.hpp \
+/usr/local/webots/include/controller/cpp/webots/Motor.hpp \
+/usr/local/webots/include/controller/cpp/webots/Device.hpp \
+/usr/local/webots/include/controller/c/webots/types.h \
+/usr/local/webots/include/controller/cpp/webots/Keyboard.hpp
